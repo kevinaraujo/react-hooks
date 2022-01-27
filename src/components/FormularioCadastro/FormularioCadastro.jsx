c99e86c39a1ced550f32b2a7bd32d9e28fb60416
Kevin Araújo <kfa_34@hotmail.com>
2022-01-27 18:32:52 -0300

    Added switch case to change page form.

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,15 +1,32 @@
+import { Typography } from "@material-ui/core";
 import React, { useState } from "react";
+import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
-function FormularioCadastro({aoEnviar, validarCPF}) {
- 
+function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(0)
+
+  function formularioAtual(etapa) {
+    switch(etapa) {
+      case 0:
+        return <DadosUsuario/>
+      case 1:
+        return  <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
+      case 2:
+        return <DadosEntrega/>
+      default:
+        return <Typography>Error</Typography>
+    }
+  }
+
   return (  
     <>
-     <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-     <DadosUsuario/>
+      { formularioAtual(etapaAtual) }
     </>
   );
 }
+
+
 
 export default FormularioCadastro;
